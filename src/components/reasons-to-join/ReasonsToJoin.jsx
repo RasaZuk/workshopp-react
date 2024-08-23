eed33b4fdff193716544f681b561d38ae146e4be
Rasa <rasa.zukauskaite@gmail.com>
2024-08-23 17:04:54 +0300

    more styling

--- src/components/reasons-to-join/ReasonsToJoin.jsx
+++ src/components/reasons-to-join/ReasonsToJoin.jsx
@@ -8,18 +8,26 @@ import four from './img/four.webp';
 export function Reasons() {
     return (
         <section className={style.reasonsList}>
-            <Reason title="Enjoy on your TV"
+            <div>
+               <Reason title="Enjoy on your TV"
             description="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more." 
-            icon={one} alt='one'/>
+            icon={one} alt='one'/> 
+            </div>
+            <div>
             <Reason title="Download your shows to watch offline"
             description="Save your favorites easily and always have something to watch."
-            icon={two} alt='two'/>
-            <Reason title="Watch everywhere"
+            icon={two} alt='two'/>    
+            </div>
+            <div>
+               <Reason title="Watch everywhere"
             description="Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV."
-            icon={three} alt='three'/>
-            <Reason title="Create profiles for kids"
+            icon={three} alt='three'/> 
+            </div>
+            <div>
+              <Reason title="Create profiles for kids"
             description="Send kids on adventures with their favorite characters in a space made just for them — free with your membership."
-            icon={four} alt='four'/>
+            icon={four} alt='four'/>  
+            </div>
         </section>
     );
 }
